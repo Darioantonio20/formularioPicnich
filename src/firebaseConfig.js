8c59a10a8aa6acd6dfc361ef502a651df596c362
Darío Antonio Gutiérrez Álvarez <darioantoniogutierrezalvarez@MacBook-Air-de-Dario.local>
2024-06-07 21:27:41 -0600

    [ADD] primer formulario

---
+++ src/firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRusFhR0SQPepf_AvPBjfYlN0qw9_SVSY",
+  authDomain: "picnich-badad.firebaseapp.com",
+  projectId: "picnich-badad",
+  storageBucket: "picnich-badad.appspot.com",
+  messagingSenderId: "329735352856",
+  appId: "1:329735352856:web:8849e551e68ed9f9ec496d",
+  measurementId: "G-J1KCY3N0DJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { app, analytics, db };
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import Formulario1 from './Formulario1.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/formulario1" element={<Formulario1 />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-)
+);
