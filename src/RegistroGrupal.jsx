678b69ea5ac3a717a10c0f10b00214b9fda459d6
Darío Antonio Gutiérrez Álvarez <darioantoniogutierrezalvarez@MacBook-Air-de-Dario.local>
2024-06-10 13:40:17 -0600

    [FIX] two forms

--- src/RegistroGrupal.jsx
+++ src/RegistroGrupal.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { db } from './firebaseConfig';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import './style/Formulario1.css';
@@ -6,45 +7,26 @@ import './style/App.css';
 
 function RegistroGrupal() {
   const [formData, setFormData] = useState({
-    nombreResponsable: '',
-    emailResponsable: '',
-    telefonoResponsable: '',
-    numAsistentes0a5hombre: '',
-    numAsistentes6a11hombre: '',
-    numAsistentes12a18hombre: '',
-    numAsistentes19a26hombre: '',
-    numAsistentes27a59hombre: '',
-    numAsistentes60mashombre: '',
-    numAsistentes0a5mujer: '',
-    numAsistentes6a11mujer: '',
-    numAsistentes12a18mujer: '',
-    numAsistentes19a26mujer: '',
-    numAsistentes27a59mujer: '',
-    numAsistentes60masmujer: '',
-    numPersonasGrupo: '',
-    paisOrigen: '',
-    estadoRadican: '',
-    ciudadRadican: '',
+    Nombre: '',
+    mail: '',
+    phone: '',
+    Hombres_0_5: '',
+    Hombres_6_11: '',
+    Hombres_12_18: '',
+    Hombres_19_26: '',
+    Hombres_27_59: '',
+    Hombres_60: '',
+    Mujeres_0_5: '',
+    Mujeres_6_11: '',
+    Mujeres_12_18: '',
+    Mujeres_19_26: '',
+    Mujeres_27_59: '',
+    Mujeres_60: '',
+    total: '',
+    Pais: '',
+    Estado: '',
+    Ciudad: '',
   });
-
-  useEffect(() => {
-    const fetchDocument = async () => {
-      try {
-        const docRef = doc(db, 'asistentes', 'grupo-id'); // Reemplaza 'grupo-id' con el ID adecuado
-        const docSnap = await getDoc(docRef);
-        
-        if (docSnap.exists()) {
-          setFormData(docSnap.data());
-        } else {
-          console.error('No such document!');
-        }
-      } catch (error) {
-        console.error('Error fetching document:', error);
-      }
-    };
-
-    fetchDocument();
-  }, []);
 
   const estados = [
     "Aguascalientes", "Baja California", "Baja California Sur", "Campeche", "Chiapas", 
@@ -69,6 +51,56 @@ function RegistroGrupal() {
     "Reino Unido", "Japón", "China", "India", "Australia"
   ];
 
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const key = searchParams.get('key');
+
+  useEffect(() => {
+    const fetchDocument = async () => {
+      try {
+        if (key) {
+          console.log('Key found in URL:', key); // Debugging
+          const docRef = doc(db, 'registrados', key);
+          const docSnap = await getDoc(docRef);
+          
+          if (docSnap.exists()) {
+            const data = docSnap.data();
+            console.log('Document data:', data); // Debugging
+            setFormData({
+              Nombre: data['Nombre.1'] || '',
+              mail: data['mail'] || '',
+              phone: data['phone.1'] || '',
+              Hombres_0_5: data['Hombres_0_5']?.toString() || '',
+              Hombres_6_11: data['Hombres_6_11']?.toString() || '',
+              Hombres_12_18: data['Hombres_12_18']?.toString() || '',
+              Hombres_19_26: data['Hombres_19_26']?.toString() || '',
+              Hombres_27_59: data['Hombres_27_59']?.toString() || '',
+              Hombres_60: data['Hombres_60']?.toString() || '',
+              Mujeres_0_5: data['Mujeres_0_5']?.toString() || '',
+              Mujeres_6_11: data['Mujeres_6_11']?.toString() || '',
+              Mujeres_12_18: data['Mujeres_12_18']?.toString() || '',
+              Mujeres_19_26: data['Mujeres_19_26']?.toString() || '',
+              Mujeres_27_59: data['Mujeres_27_59']?.toString() || '',
+              Mujeres_60: data['Mujeres_60']?.toString() || '',
+              total: data['total']?.toString() || '',
+              Pais: data['Pais.1'] || '',
+              Estado: data['Estado.1'] || '',
+              Ciudad: data['Ciudad.1'] || '',
+            });
+          } else {
+            console.error('No such document!');
+          }
+        } else {
+          console.error('No key provided in the URL');
+        }
+      } catch (error) {
+        console.error('Error fetching document:', error);
+      }
+    };
+
+    fetchDocument();
+  }, [key]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
@@ -77,7 +109,7 @@ function RegistroGrupal() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await setDoc(doc(db, 'asistentes', 'grupo-id'), formData, { merge: true }); // Reemplaza 'grupo-id' con el ID adecuado
+      await setDoc(doc(db, 'asistentes', key), formData, { merge: true });
       console.log('Document successfully written!');
     } catch (error) {
       console.error('Error writing document:', error);
@@ -96,54 +128,54 @@ function RegistroGrupal() {
       <form className="registration-form" onSubmit={handleSubmit}>
         <div className="form-group">
           <label className="form-labelcito ">Nombre completo del responsable del grupo:</label>
-          <input className="input" type="text" name="nombreResponsable" value={formData.nombreResponsable} onChange={handleChange} required />
+          <input className="input" type="text" name="Nombre" value={formData.Nombre} onChange={handleChange} required />
         </div>
         <div className="form-group">
           <label className="form-labelcito ">Correo electrónico del responsable del grupo: <b>(Verifica el dato porque a esta dirección enviaremos el PASE de acceso)</b></label>
-          <input className="input" type="email" name="emailResponsable" value={formData.emailResponsable} onChange={handleChange} required />
+          <input className="input" type="email" name="mail" value={formData.mail} onChange={handleChange} required />
         </div>
         <div className="form-group">
           <label className="form-labelcito ">Número telefónico del responsable del grupo (opcional):</label>
-          <input className="input" type="number" name="telefonoResponsable" value={formData.telefonoResponsable} onChange={handleChange} />
+          <input className="input" type="number" name="phone" value={formData.phone} onChange={handleChange} />
         </div>
         <div className="form-group">
           <label className="form-labelcito ">Número de personas que forman el grupo (solo número):</label>
-          <input className="input" type="number" name="numPersonasGrupo" value={formData.numPersonasGrupo} onChange={handleChange} required />
+          <input className="input" type="number" name="total" value={formData.total} onChange={handleChange} required />
         </div>
         <div className="form-group">
           <label className="form-labelcito ">Número de asistentes de 0 a 5 años: <b>(Si no hay asistentes en este rango, escribe 0)</b></label>
-          <input className="input" placeholder='Hombre' type="number" name="numAsistentes0a5hombre" value={formData.numAsistentes0a5hombre} onChange={handleChange} required />
-          <input className="input" placeholder='Mujer' type="number" name="numAsistentes0a5mujer" value={formData.numAsistentes0a5mujer} onChange={handleChange} required />
+          <input className="input" placeholder='Hombre' type="number" name="Hombres_0_5" value={formData.Hombres_0_5} onChange={handleChange} required />
+          <input className="input" placeholder='Mujer' type="number" name="Mujeres_0_5" value={formData.Mujeres_0_5} onChange={handleChange} required />
         </div>
         <div className="form-group">
           <label className="form-labelcito ">Número de asistentes de 6 a 11 años: <b>(Si no hay asistentes en este rango, escribe 0)</b></label>
-          <input className="input" placeholder='Hombre' type="number" name="numAsistentes6a11hombre" value={formData.numAsistentes6a11hombre} onChange={handleChange} required />
-          <input className="input" placeholder='Mujer' type="number" name="numAsistentes6a11mujer" value={formData.numAsistentes6a11mujer} onChange={handleChange} required />
+          <input className="input" placeholder='Hombre' type="number" name="Hombres_6_11" value={formData.Hombres_6_11} onChange={handleChange} required />
+          <input className="input" placeholder='Mujer' type="number" name="Mujeres_6_11" value={formData.Mujeres_6_11} onChange={handleChange} required />
         </div>
         <div className="form-group">
           <label className="form-labelcito ">Número de asistentes de 12 a 18 años: <b>(Si no hay asistentes en este rango, escribe 0)</b></label>
-          <input className="input" placeholder='Hombre' type="number" name="numAsistentes12a18hombre" value={formData.numAsistentes12a18hombre} onChange={handleChange} required />
-          <input className="input" placeholder='Mujer' type="number" name="numAsistentes12a18mujer" value={formData.numAsistentes12a18mujer} onChange={handleChange} required />
+          <input className="input" placeholder='Hombre' type="number" name="Hombres_12_18" value={formData.Hombres_12_18} onChange={handleChange} required />
+          <input className="input" placeholder='Mujer' type="number" name="Mujeres_12_18" value={formData.Mujeres_12_18} onChange={handleChange} required />
         </div>
         <div className="form-group">
           <label className="form-labelcito ">Número de asistentes de 19 a 26 años: <b>(Si no hay asistentes en este rango, escribe 0)</b></label>
-          <input className="input" placeholder='Hombre' type="number" name="numAsistentes19a26hombre" value={formData.numAsistentes19a26hombre} onChange={handleChange} required />
-          <input className="input" placeholder='Mujer' type="number" name="numAsistentes19a26mujer" value={formData.numAsistentes19a26mujer} onChange={handleChange} required />
+          <input className="input" placeholder='Hombre' type="number" name="Hombres_19_26" value={formData.Hombres_19_26} onChange={handleChange} required />
+          <input className="input" placeholder='Mujer' type="number" name="Mujeres_19_26" value={formData.Mujeres_19_26} onChange={handleChange} required />
         </div>
         <div className="form-group">
           <label className="form-labelcito ">Número de asistentes de 27 a 59 años: <b>(Si no hay asistentes en este rango, escribe 0)</b></label>
-          <input className="input" placeholder='Hombre' type="number" name="numAsistentes27a59hombre" value={formData.numAsistentes27a59hombre} onChange={handleChange} required />
-          <input className="input" placeholder='Mujer' type="number" name="numAsistentes27a59mujer" value={formData.numAsistentes27a59mujer} onChange={handleChange} required />
+          <input className="input" placeholder='Hombre' type="number" name="Hombres_27_59" value={formData.Hombres_27_59} onChange={handleChange} required />
+          <input className="input" placeholder='Mujer' type="number" name="Mujeres_27_59" value={formData.Mujeres_27_59} onChange={handleChange} required />
         </div>
         <div className="form-group">
           <label className="form-labelcito ">Número de asistentes de 60 años o más: <b>(Si no hay asistentes en este rango, escribe 0)</b></label>
-          <input className="input" placeholder='Hombre' type="number" name="numAsistentes60mashombre" value={formData.numAsistentes60mashombre} onChange={handleChange} required />
-          <input className="input" placeholder='Mujer' type="number" name="numAsistentes60masmujer" value={formData.numAsistentes60masmujer} onChange={handleChange} required />
+          <input className="input" placeholder='Hombre' type="number" name="Hombres_60" value={formData.Hombres_60} onChange={handleChange} required />
+          <input className="input" placeholder='Mujer' type="number" name="Mujeres_60" value={formData.Mujeres_60} onChange={handleChange} required />
         </div>
         <div className="container">
           <div className="form-group">
             <label className="form-labelcito">País de origen:</label>
-            <select className="input" name="paisOrigen" value={formData.paisOrigen} onChange={handleChange} required>
+            <select className="input" name="Pais" value={formData.Pais} onChange={handleChange} required>
               <option value="">Seleccione su país</option>
               {paises.map((pais, index) => (
                 <option key={index} value={pais}>{pais}</option>
@@ -152,7 +184,7 @@ function RegistroGrupal() {
           </div>
           <div className="form-group">
             <label className="form-labelcito">Estado donde radican:</label>
-            <select className="input" name="estadoRadican" value={formData.estadoRadican} onChange={handleChange} required>
+            <select className="input" name="Estado" value={formData.Estado} onChange={handleChange} required>
               <option value="">Seleccione su estado</option>
               {estados.map((estado, index) => (
                 <option key={index} value={estado}>{estado}</option>
@@ -161,7 +193,7 @@ function RegistroGrupal() {
           </div>
           <div className="form-group">
             <label className="form-labelcito">Ciudad donde radican:</label>
-            <select className="input" name="ciudadRadican" value={formData.ciudadRadican} onChange={handleChange} required>
+            <select className="input" name="Ciudad" value={formData.Ciudad} onChange={handleChange} required>
               <option value="">Seleccione su ciudad</option>
               {ciudades.map((ciudad, index) => (
                 <option key={index} value={ciudad}>{ciudad}</option>
